9450fca69c6b3b69d63bf64bb9dbeded70394fc6
Crowdbotics <team@crowdbotics.com>
2023-03-17 17:02:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-wood-39430",
+  url: "https://frosty-wood-39430.botics.co",
+  api: "https://frosty-wood-39430.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
